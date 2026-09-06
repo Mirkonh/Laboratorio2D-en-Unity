859ac820c7b8a805646383d5e9f30071051256c1
Mirkonh <70263705@continental.edu.pe>
2026-09-05 19:08:42 -0500

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidad = 5f;
+    public float fuerzaSalto = 8f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Movimiento izquierda y derecha
+        float movimiento = Input.GetAxisRaw("Horizontal");
+
+        rb.linearVelocity = new Vector2(
+            movimiento * velocidad,
+            rb.linearVelocity.y
+        );
+
+        // Saltar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector2(
+                rb.linearVelocity.x,
+                fuerzaSalto
+            );
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Laboratorio2D en Unity
+Proyecto de laboratorio 2D en Unity..
